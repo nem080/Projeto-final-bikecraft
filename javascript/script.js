77c3f191cbf4b0b7e9fc06b52f4a798e3cef0d52
Rogisvaldo Js <73315533+nem080@users.noreply.github.com>
2023-09-26 22:55:02 -0300

    incluido o javaScript na tela de bicicletas

--- javascript/script.js
+++ javascript/script.js
@@ -22,3 +22,66 @@ function ativarProduto(parametro) {
 }
 
 parametros.forEach(ativarProduto);
+
+
+
+// galeria de imgs das bicicletas Nimbus.
+// const galeria = document.querySelectorAll('.bicicleta-imagens img');
+// const galeriaContainer = document.querySelector('.bicicleta-nibus'); 
+// // console.log(galeria, galeriaContainer);
+
+// function trocarImagem(event) {
+//     const img = event.currentTarget;
+//     galeriaContainer.prepend(img)
+//     console.log(img);
+// }
+
+// function eventosGaleria(img) {
+//     img.addEventListener('click', trocarImagem);
+// }
+
+// galeria.forEach(eventosGaleria);
+
+// const galeria = document.querySelectorAll('.bicicleta-imagens img');
+// const destaqueImagem = document.querySelector('.bicicleta-destaque img');
+
+// function trocarImagem(event) {
+//     const img = event.currentTarget;
+//     const src = img.getAttribute('src');
+//     const destaqueSrc = destaqueImagem.getAttribute('src');
+    
+
+//     img.setAttribute('src', destaqueSrc);
+//     destaqueImagem.setAttribute('src', src);
+// }
+
+// galeria.forEach(function (img) {
+//     img.addEventListener('click', trocarImagem);
+// });
+
+
+
+const galeria = document.querySelectorAll('.bicicleta-imagens img');
+const destaqueImagem = document.querySelector('.bicicleta-destaque img');
+
+function trocarImagem(event) {
+    const img = event.currentTarget;
+    const src = img.getAttribute('src');
+    const destaqueSrc = destaqueImagem.getAttribute('src');
+
+    // Verificar a largura da tela
+    const media = window.matchMedia("(min-width: 9000px)");
+
+    if (media.matches) {
+        // A tela tem uma largura de pelo menos 1000px, então podemos continuar a troca
+        img.setAttribute('src', destaqueSrc);
+        destaqueImagem.setAttribute('src', src);
+    } else {
+        // A tela está no modo responsivo, não fazemos a troca
+        alert("A tela está no modo responsivo, a troca de imagem está desabilitada.");
+    }
+}
+
+galeria.forEach(function (img) {
+    img.addEventListener('click', trocarImagem);
+});
--- javascript/seguros.js
+++ javascript/seguros.js
@@ -1,20 +1,40 @@
+// const perguntas = document.querySelectorAll('.pergunta');
+
+// perguntas.forEach(pergunta => {
+//     pergunta.addEventListener('click', () => {
+//         const resposta = pergunta.nextElementSibling;
+//         if (resposta.style.display === 'block') {
+//             resposta.style.display = 'none';
+            
+//         } else{
+//             resposta.style.display = 'block';
+//             pergunta.style.borderRadius = "8px 8px 0 0"
+//             resposta.style.backgroundColor = 'var(--cor-10)'
+//             resposta.style.borderRadius =" 0 0 8px 8px"
+//             resposta.style.color = "var(--cor-2)"
+//             // setaAbrir.style.display = 'none'
+//             // setaFechar.style.display ='inline-block'
+//             resposta.classList.add(seta-ativa);
+
+//         }
+//     });
+// });
+
 const perguntas = document.querySelectorAll('.pergunta');
 
 perguntas.forEach(pergunta => {
-    pergunta.addEventListener('click', () => {
+    pergunta.addEventListener('click', (event) => {
         const resposta = pergunta.nextElementSibling;
         if (resposta.style.display === 'block') {
             resposta.style.display = 'none';
-            
-        } else{
+            pergunta.classList.remove('aberta'); // Remova a classe quando a resposta estiver oculta
+        } else {
             resposta.style.display = 'block';
             pergunta.style.borderRadius = "8px 8px 0 0"
             resposta.style.backgroundColor = 'var(--cor-10)'
-            resposta.style.borderRadius =" 0 0 8px 8px"
+            resposta.style.borderRadius = " 0 0 8px 8px"
             resposta.style.color = "var(--cor-2)"
-            setaAbrir.style.display = 'none'
-            setaFechar.style.display ='inline-block'
+            pergunta.classList.add('aberta'); // Adicione a classe quando a resposta estiver visível
         }
     });
 });
-
